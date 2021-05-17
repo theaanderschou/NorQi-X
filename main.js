c7f6c41ef251d2724a1f14d418a13052b960102b
theaanderschou <74912677+theaanderschou@users.noreply.github.com>
2021-05-17 14:12:56 +0200

    hero + burger done

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const navslide = () => {
     const menu = document.querySelector('.menu');
     const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
+    const nav = document.querySelector('NAV');
     const navLinks = document.querySelectorAll('.nav-links li');
 
     burger.addEventListener('click',()=>{
